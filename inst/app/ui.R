56a15f15d3e1d593d6f37a56f258eec5a11bb0b8
Lukas Fuchs <lukas.fuchs@inwt-statistics.de>
2024-08-15 15:44:42 +0200

    first example app

---
+++ inst/app/ui.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(oxcAAR)
+
+tagList(
+  navbarPage(
+    title = paste("oxcAARS", packageVersion("oxcAARS")),
+    theme = shinythemes::shinytheme("flatly"),
+    position = "fixed-top",
+    collapsible = TRUE,
+    id = "tab",
+    tabPanel(
+      title = "Tab",
+      sidebarLayout(
+        sidebarPanel(
+          width = 2,
+          actionButton("example", "Create Example")
+        ),
+        mainPanel(
+          plotOutput("example_plot_1"),
+          plotOutput("example_plot_2")
+        ),
+      )
+    )
+  ),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  )
+)
